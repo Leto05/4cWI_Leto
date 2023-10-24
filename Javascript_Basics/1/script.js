2647d3c7f209f3d2b4702265a825f73468ef5426
Leto05 <leto.harm@student.htldornbirn.at>
2023-10-24 09:26:51 +0200

    JSS angefangen

---
+++ Javascript_Basics/1/script.js
@@ -0,0 +1,37 @@
+console.log("Servus");
+
+let a = 7;
+let b = 8;
+let c = 10;
+// Berechne folgendes und gib das Eregebnis über console.log aus
+// a + b + c
+// (a + b) / c
+// c -a + b
+// a + b + c * 3
+// (a + b + c)/4
+
+// Ändere die Variablen auf folgende Werte
+// a = 3
+// b = a + 3
+// c = c -3
+// gib die neuen Werte der Variablen aus
+
+let result = a+b+c;
+console.log(result);
+
+result = (a+b)/c;
+console.log(result);
+
+result = c-a+b;
+console.log(result);
+
+result = a+b+c*3;
+console.log(result);
+
+result = (a+b+c)/4;
+console.log(result);
+
+a=3;
+b=a+3;
+c=c-3;
+console.log("a= " + a +" b= " + b + " c= " + c)
---
+++ Javascript_Basics/2/script.js
@@ -0,0 +1,10 @@
+let a = 17.4;
+let b= 3.9;
+let result = a/b;
+
+console.log("The result of "+a+" divided by "+b+" is "+result);
+
+let vorname = "Max";
+let nachname = "Mustermann";
+
+console.log(" Mein Name ist: "+vorname+" "+nachname)
