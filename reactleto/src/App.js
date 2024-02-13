4169655ffb20d9fdc5f73ee83f0771e33a1fb773
Leto05 <leto.harm@student.htldornbirn.at>
2024-02-13 09:22:01 +0100

    Router

--- reactleto/src/App.js
+++ reactleto/src/App.js
@@ -1,14 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import ReturnHTML from './components/PersonList';
 import PeopleContainer from './components/peopleContainer';
 //jsx Sprache
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/People",
+    element: <div><PeopleContainer/></div>
+  },
+  {
+    path: "/",
+    element: <div>Hello</div>,
+  },
+]);
+
 function App() {
   return (
-    <div className="App">
-      <PeopleContainer/>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
 export default App;
+/*<div className="App">
+<PeopleContainer/>
+</div>*/
--- reactleto/src/components/peopleContainer.js
+++ reactleto/src/components/peopleContainer.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Card from './Card'
 
-export default function peopleContainer() {
+export default function PeopleContainer() {
+  const [people, setPeople] = useState([]);
+
+  const [filteredpeople, setFilteredPeople]= useState([]);
+
+  useEffect(()=>{
+    fetch("https://659feda05023b02bfe8ad7b6.mockapi.io/person").then(
+      (res) => res.json().then((data)=>{setPeople(data);
+        setFilteredPeople(data);
+      }));
+  },[]);
+  
+const filterPeople = (filter)=>{
+    let filtered= people.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+    setFilteredPeople(filtered);
+}
+
   return (
-    <div>
-        <h1>People</h1>
-        <div>Person</div>
+    <div className=''>
+      <div className='fixed border mb-4 bg-green-400 w-full h-28 p-4'>
+      <h1>People</h1>
+        <input className="border p-4"type="text" placeholder="Search" onChange={(el)=>{
+            console.log(el.target.value);
+            filterPeople(el.target.value)
+        }}/>
+      </div>
+      <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 p-8 pt-32'>
+          {filteredpeople.map(person => 
+          {
+            return <Card name={person.name} title={person.jobtitle} imageUri={person.avatar}/>
+          })}
+          <Card name="Hans" title="CEO" imageUri="https://picsum.photos/200/300"/>
+          <Card name="Hans" title="CEO" imageUri="https://picsum.photos/200/300"/>
+          <Card name="Hans" title="CEO" imageUri="https://picsum.photos/200/300"/>
+      </div>
     </div>
   )
 }
